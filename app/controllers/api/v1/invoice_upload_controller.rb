dc0a5266f3cde3d08f029260007df1a11011a62e
astr0surf3r <astr0surf3r@gmail.com>
2022-09-07 20:50:06 +0200

    refactor Invoice Upload controller

--- app/controllers/api/v1/invoice_upload_controller.rb
+++ app/controllers/api/v1/invoice_upload_controller.rb
@@ -6,7 +6,10 @@ module Api
       def create
         @files = params[:files]
         begin
-          iterate_files(@files)
+          files.each do |file|
+            create_invoice(file)
+          end
+
           render json: { info: 'invoices added to database' }.to_json, status: :ok
         rescue Exception => e
           Rails.logger.debug { "Something Went Wrong #{e}" }
@@ -16,24 +19,22 @@ module Api
 
       private
 
-      def iterate_files(files)
-        files.each do |file|
-          doc = Nokogiri::XML.parse(file)
-          invoice = Invoice.find_or_create_by(
-            invoice_uuid: doc.xpath('//invoice_uuid').text,
-            emitsdsdter: doc.xpath('//emitter//name').text,
-            emitter_rfc: doc.xpath('//emitter//rfc').text,
-            receiver: doc.xpath('//receiver//name').text,
-            receiver_rfc: doc.xpath('//receiver//rfc').text,
-            amount: doc.xpath('//amount//cents').text.to_i,
-            currency: doc.xpath('//amount//currency').text,
-            emitted_at: doc.xpath('//emitted_at').text,
-            expires_at: doc.xpath('//expires_at').text,
-            signed_at: doc.xpath('//signed_at').text,
-            cfdi_digital_stamp: doc.xpath('//cfdi_digital_stamp').text,
-            user_id: @current_api_user.id
-          )
-        end
+      def create_invoice(file)
+        doc = Nokogiri::XML.parse(file)
+        invoice = Invoice.find_or_create_by(
+          invoice_uuid: doc.xpath('//invoice_uuid').text,
+          emitsdsdter: doc.xpath('//emitter//name').text,
+          emitter_rfc: doc.xpath('//emitter//rfc').text,
+          receiver: doc.xpath('//receiver//name').text,
+          receiver_rfc: doc.xpath('//receiver//rfc').text,
+          amount: doc.xpath('//amount//cents').text.to_i,
+          currency: doc.xpath('//amount//currency').text,
+          emitted_at: doc.xpath('//emitted_at').text,
+          expires_at: doc.xpath('//expires_at').text,
+          signed_at: doc.xpath('//signed_at').text,
+          cfdi_digital_stamp: doc.xpath('//cfdi_digital_stamp').text,
+          user_id: @current_api_user.id
+        )
       end
     end
   end
